c0f964c78b33bc5a53abae87554946097ee7d779
colombohenrique <jhbcolombo@gmail.com>
2022-02-01 00:35:07 -0300

    people model fixes

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/colombohenrique/rollin-api/database"
 	"github.com/colombohenrique/rollin-api/routes"
 )
 
 func main() {
-	//database.ConectaComBancoDeDados()
+	fmt.Println("Tentando conectar...")
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando API")
 	routes.HandleRequest()
 }
