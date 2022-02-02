2c94a8e4b0c21414f02e10bdd59251da2eed1300
colombohenrique <jhbcolombo@gmail.com>
2022-02-01 23:17:56 -0300

    Trying correct error on controller users

--- controllers/users.go
+++ controllers/users.go
@@ -20,6 +20,19 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	id := vars["id"]
 	var user models.User
 	database.DB.First(&user, id)
+	// rows, err := database.DB.Table("users").Select("*").Joins("left join persons on persons.id = users.person_id").Where("users.id = " + id).Rows()
+
+	// if err != nil {
+	// 	fmt.Println("batata error: ", err)
+	// }
+
+	// for rows.Next() {
+	// 	fmt.Println(rows.Scan(user))
+	// 	rows.Scan(&user)
+	// 	fmt.Println(user)
+	// 	rows.Scan(&person)
+	// 	fmt.Println(person)
+	// }
 	json.NewEncoder(w).Encode(user)
 }
 
